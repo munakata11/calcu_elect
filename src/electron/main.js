6920d46375f1e3d1754d769cf5a93a167f6acff0
munakata11 <hys.be.at@gmail.com>
2024-12-04 22:00:48 +0900

    electronウィンドウ

--- src/electron/main.js
+++ src/electron/main.js
@@ -16,8 +16,8 @@ function handlePythonProcessError(error) {
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 820,
-    height: 719,
+    width: 805,
+    height: 680,
     minWidth: 400,
     minHeight: 500,
     webPreferences: {
